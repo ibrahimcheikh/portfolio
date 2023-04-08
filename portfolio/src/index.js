f3ddfa20c9aea6618693405b1c1c05a19bcdab44
ibrahimcheikh <ibrahimcheikh55@gmail.com>
2023-04-07 23:58:41 -0400

    switiching to my desktop

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,8 +1,17 @@
-import Spline from '@splinetool/react-spline';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/35EA81ZBkDk77Thw/scene.splinecode" />
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
