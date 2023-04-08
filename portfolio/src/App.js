e3c3ba66bc8d77cca7c253bc99fe60c7cf292580
ibrahimcheikh <ibrahimcheikh55@gmail.com>
2023-04-08 00:55:14 -0400

    getting there

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
 import Spline from '@splinetool/react-spline';
 
+
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/35EA81ZBkDk77Thw/scene.splinecode" />
+
+      <Spline className ="spline" scene="https://prod.spline.design/35EA81ZBkDk77Thw/scene.splinecode" />
+
   );
 }
+
