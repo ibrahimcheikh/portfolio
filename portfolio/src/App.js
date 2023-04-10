afc3a036456b21ec3388e91a3fdcf295059b1134
ibrahimcheikh <ibrahimcheikh55@gmail.com>
2023-04-10 02:34:50 -0400

    added logo and some fixes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,36 @@
 import Spline from '@splinetool/react-spline';
 import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './styles.css'; // Import the CSS file
 export default function App() {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScrollPosition(window.scrollY);
+    });
+  }, []);
   return (
     <Wrapper>
+
       <Spline className="spline" scene="https://prod.spline.design/C-CAjXRA9cJOnhpf/scene.splinecode" />
-      <Content><h1>Hello and welcome to my Website</h1>
-        <p>hellof dshfdsjf sdjl; sdj fjfjklsf djkls fjklsf jkls fjkl;sfjk; lsjkl sfdjkl;sfjkl sfdjklfsdjklsdjklfsd
-          fdsjhkjks fdjlkfsd jklfsd jlsfd ljkfsd jklfsd lkfs djlsfd jlsf
+      <Content>
+        <Menu>
+          <li><img src={logo} alt="Logo" /></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Education</a></li>
+          <li><a href="/">Experience</a></li>
+
+        </Menu>
+
+        <h1 style={{ opacity: scrollPosition > 100 ? 0 : 1 }}>Ibrahim Cheikh</h1>
+        <p style={{ opacity: scrollPosition > 100 ? 0 : 1 }}>Imagine you are a character in a story,
+          and your mission is to create something
+          that will inspire others. What would you create?
+          How would you do it? Think about the impact you
+          want to have on the world and let your creativity
+          guide you.
         </p>
 
       </Content>
@@ -18,7 +41,7 @@ export default function App() {
 
 const Wrapper = styled.div`
   font-family: "Spline sans",sans-serif;  
-  font-size: 24px;
+  font-size: 16px;
   color: white;
   .spline {
     position: absolute;
@@ -33,22 +56,51 @@ top:30px;
 display: flex;
 flex-direction:column;
 
-.h1{
+h1{
   font-family: "Spline sans",sans-serif;  
   font-size: 100px;
   font-weight:bold;
   margin:0;
   max-width: 500px;
-}
 
-.p{
+  transition: opacity 0.5s ease-in-out;
+}
+p{
   font-weight:normal;
   line-height:150%;
   max-width:380px;
-}
 
+  transition: opacity 0.5s ease-in-out 0.2s;
+}
 h1,p{
   margin: 0 30px 0 100px;
 }
-
 `;
+
+const Menu = styled.ul`
+display: flex;
+gap:30px;
+align-items:center
+
+padding: 0;
+li{
+  list-style: none;
+  margin:55px;
+  img {
+    height: 200px;
+  }
+}
+a{
+  text-decoration:none;
+  color:white;
+  padding: 8px 100px;
+  border: 1px solid rgba(255,255,255,0);
+  transition: 1s;
+  border-radius:14px;
+
+  :hover{
+    border:1px solid rgba(255,255,255,0.2)
+  }
+
+}
+`
